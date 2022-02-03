05b17ac2d81e70f0dec37665f984e42a8ada1b92
Geri <luga.geri@gmail.com>
2022-02-03 19:59:30 +0100

    Can chat with a delay of 2 sec, added autoscroll at the end of the messages on new message

--- src/pages/Conversation.jsx
+++ src/pages/Conversation.jsx
@@ -9,14 +9,14 @@ const Conversation = () => {
   const currentUser = useStore((store) => store.currentUser);
   const conversations = useStore((store) => store.conversations);
   const allUsers = useStore((store) => store.allUsers);
-  const [currentConversationMessages, setCurrentConversationMessages] = useState([]);
-//   const currentConversationMessages = useStore(
-//     (store) => store.currentConversationMessages
-//   );
-//   const setCurrentConversationMessages = useStore(
-//     (store) => store.setCurrentConversationMessages
-//   );
-
+  const [currentConversationMessages, setCurrentConversationMessages] =
+    useState([]);
+  //   const currentConversationMessages = useStore(
+  //     (store) => store.currentConversationMessages
+  //   );
+  //   const setCurrentConversationMessages = useStore(
+  //     (store) => store.setCurrentConversationMessages
+  //   );
 
   const navigate = useNavigate();
   const params = useParams();
@@ -24,9 +24,17 @@ const Conversation = () => {
     !currentUser && navigate("/");
   }, []);
 
+  //   useEffect(() => {
+  //     //scroll list of messages to the bottom after 70ms
+  //     setTimeout((e) => {
+  //       const messagesUl = document.querySelector(".list-of-messages");
+  //       messagesUl.scrollTop = messagesUl.scrollHeight;
+  //     }, 70);
+  //   }, []);
+
   //find the conversation with the current user and the param user
-  let currentConversation = null
-  currentConversation= conversations.find((convo) => {
+  let currentConversation = null;
+  currentConversation = conversations.find((convo) => {
     if (
       allUsers.find((user) => {
         return user.username === params.username;
@@ -39,21 +47,46 @@ const Conversation = () => {
     }
     return false;
   });
-  console.log(currentConversation)
-  let userTalkingTo=null
-  if(currentConversation){userTalkingTo = allUsers.find(
-    (user) =>
-      user.id ===
-      (currentConversation.userId === currentUser.id
-        ? currentConversation.participantId
-        : currentConversation.userId)
-  );}
+  console.log(currentConversation);
+  let userTalkingTo = null;
+  if (currentConversation) {
+    userTalkingTo = allUsers.find(
+      (user) =>
+        user.id ===
+        (currentConversation.userId === currentUser.id
+          ? currentConversation.participantId
+          : currentConversation.userId)
+    );
+  }
   useEffect(() => {
     //fetch the messages in that conversation
-    if (currentUser&&currentConversation)
-      fetchConversationMessages(currentConversation.id).then((serverMessages) =>
-        setCurrentConversationMessages(serverMessages)
+    let intervalId;
+    if (currentUser && currentConversation) {
+      //fetch as soon as page loads
+      fetchConversationMessages(currentConversation.id).then(
+        (serverMessages) => {
+          setCurrentConversationMessages(serverMessages);
+          const messagesUl = document.querySelector(".list-of-messages");
+          messagesUl.scrollTop = messagesUl.scrollHeight;
+        }
       );
+      //fetch messages periodically and also scroll to bottom every time
+      intervalId = setInterval((e) => {
+        fetchConversationMessages(currentConversation.id).then(
+          (serverMessages) => {
+            //only scroll if new Messages found
+            if (currentConversationMessages.length !== serverMessages.length) {
+              const messagesUl = document.querySelector(".list-of-messages");
+              messagesUl.scrollTop = messagesUl.scrollHeight;
+            }
+            setCurrentConversationMessages(serverMessages);
+          }
+        );
+
+        console.log("interval ran");
+      }, 2000);
+    }
+    return () => clearInterval(intervalId);
   }, [currentConversation]);
 
   if (!currentUser) return <h2>Loading...</h2>;
@@ -62,29 +95,36 @@ const Conversation = () => {
     <main className="home">
       <h1 className="home-h1">hinder</h1>
       <section className="single-chat">
-        {userTalkingTo&&<section className="chat-user-info">
-          <button onClick={(e) => navigate("/chat")}>◀</button>
-          <img src={userTalkingTo.photo} alt="" />
-          <span>{userTalkingTo.username}</span>
-        </section>}
+        {userTalkingTo && (
+          <section className="chat-user-info">
+            <button onClick={(e) => navigate("/chat")}>◀</button>
+            <img src={userTalkingTo.photo} alt="" />
+            <span>{userTalkingTo.username}</span>
+          </section>
+        )}
         <section className="chat-messages">
           <ul className="list-of-messages">
-            {currentConversationMessages.length!==0&&currentConversationMessages.map((message) => (
-              <li
-                className={
-                  message.userId === currentUser.id
-                    ? "current-user-message"
-                    : ""
-                }
-                key={message.id}
-              >
-                {message.messageText}
-              </li>
-            ))}
+            {currentConversationMessages.length !== 0 &&
+              currentConversationMessages.map((message) => (
+                <li
+                  className={
+                    message.userId === currentUser.id
+                      ? "current-user-message"
+                      : "other-user-message"
+                  }
+                  key={message.id}
+                >
+                  {message.messageText}
+                </li>
+              ))}
           </ul>
+
           <form
+            className="send-msg"
             onSubmit={(e) => {
               e.preventDefault();
+              const messagesUl = document.querySelector(".list-of-messages");
+
               const message = {
                 conversationId: currentConversation.id,
                 userId: currentUser.id,
@@ -98,12 +138,19 @@ const Conversation = () => {
                     ...currentConversationMessages,
                     serverMessage,
                   ]);
+                  messagesUl.scrollTop = messagesUl.scrollHeight;
                 });
               // @ts-ignore
               e.target.reset();
             }}
           >
-            <input name="msg" type="text" />
+            <input
+              autoComplete="off"
+              style={{ textIndent: "0.5rem" }}
+              name="msg"
+              type="text"
+            />
+            <button type="submit">▶</button>
           </form>
         </section>
       </section>
